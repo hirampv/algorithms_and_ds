8177763db882be56a00ac6d3c311a2e22184e0fe
Hiram Pina Valdes <hiram.p@gmail.com>
2025-01-18 16:23:28 -0600

    Add SinglyLinkedList class

--- data_structures.py
+++ data_structures.py
@@ -8,3 +8,26 @@ class SinglyLinkedListNode:
     def __init__(self, data) -> None:
         self.data = data
         self.next = None
+
+'''
+    Class definition for a singly linked list
+    The class contains two elements:
+        head: a pointer to the first element
+        tail: a pointer to the last element
+        We could also include an integer for the list's length or size, but it would not
+        contrubute much to the list's performance or usability
+'''
+class SinglyLinkedList:
+    def __init__(self, data):
+        new_node = SinglyLinkedListNode(data)
+        self.head = new_node
+        self.tail = new_node
+
+    def print(self):
+        tmp = self.head
+        print_str = ""
+        while tmp:
+            print_str += str(tmp.data) + " -> "
+            tmp = tmp.next
+        print_str += "None"
+        print(print_str)
