494b945bb98ca9b2be81ee01c2a17bb992536a95
Hiram Pina Valdes <hiram.p@gmail.com>
2025-01-20 15:06:05 -0600

    Create sll_playground module to test out the SinglyLinkedList class

---
+++ sll_playground.py
@@ -0,0 +1,9 @@
+from data_structures import SinglyLinkedListNode, SinglyLinkedList
+
+def run():
+    my_list = SinglyLinkedList(0)
+    my_list.append(1)
+    my_list.print()
+
+if __name__ == '__main__':
+    run()
