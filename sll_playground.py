3741d18e0e708b475803916a062c5a8ef216db64
Hiram Pina Valdes <hiram.p@gmail.com>
2025-01-29 20:04:38 -0600

    Add Python 'shebang' and call to the remove method

--- sll_playground.py
+++ sll_playground.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from data_structures import SinglyLinkedListNode, SinglyLinkedList
 
 def run():
@@ -7,6 +8,18 @@ def run():
     my_list.prepend(0)
     my_list.print()
 
+    print("Removing the 0th element: ", my_list.remove(0).data)
+    my_list.print()
+
+    print("Removing the element at index 1: ", my_list.remove(1).data)
+    my_list.print()
+
+    print("Removing the element at index 4")
+    my_list.remove(4)
+    my_list.print()
+
+
+    '''
     print("Removing the element ", my_list.pop().data)
     my_list.print()
 
@@ -18,6 +31,7 @@ def run():
 
     print("Removing the element ", my_list.pop_first().data)
     my_list.print()
+    '''
 
 if __name__ == '__main__':
     run()
