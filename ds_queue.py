47a494c54330bc3802281e41c1e4bcdd02fedfa1
Hiram Pina Valdes <hiram.p@gmail.com>
2025-02-13 19:52:02 -0600

    Add enqueue() method to Queue class

--- ds_queue.py
+++ ds_queue.py
@@ -5,3 +5,21 @@ class Queue:
         newNode = SinglyLinkedListNode(value)
         self.front = newNode
         self.rear  = newNode
+
+    def enqueue(self, value):
+        newNode = SinglyLinkedListNode(value)
+        
+        if self.front is None:
+            self.front = newNode
+            self.rear  = newNode
+
+        else:
+            self.rear.next = newNode
+            self.rear      = newNode
+
+
+if __name__ == "__main__":
+    myQueue = Queue(0)
+    myQueue.enqueue(1)
+    myQueue.enqueue(2)
+    myQueue.enqueue(3)
